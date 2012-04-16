cd50634c681b2d0b4f09c0775b6606dc245bb41a
Radhika Rawat <rawat.radhika@gmail.com>
2012-04-15 20:32:15 -0700

    fixed plotting

--- Rawat_HW9/hw9.py
+++ Rawat_HW9/hw9.py
@@ -26,11 +26,8 @@ def multiprocessing_method(n_of_darts, n_of_pools = 10):
 	'''Uses 10 processes to split the work of dart_algorithm. Since the processes are independent, \
 	I split the number of darts over the number of processes, and used the pool function. Instead of \
 	looping over the number of processes, which would be time-consuming, I wrote them explicitly.'''
-	
-	global Pool_darts_in_circle,Pool_time
-	Pool_darts_in_circle = 0
-	Pool_time = 0
-	
+
+
 	pool = Pool(processes=n_of_pools)  
 	darts_per_pool = n_of_darts/n_of_pools
 
@@ -50,16 +47,16 @@ def multiprocessing_method(n_of_darts, n_of_pools = 10):
 	Pool_darts_in_circle = result1.get()[0] + result2.get()[0]+result3.get()[0] +\
 	result4.get()[0]+result5.get()[0]+result6.get()[0]+ result7.get()[0]+ \
 	result8.get()[0]+ result9.get()[0]+ result10.get()[0]
+	multiprocessing_time = end-start
 	
-
-
-	Pool_time += end-start
 	pool.close()
 	pool.join()
 	
 	pi = 4 * Pool_darts_in_circle / float(n_of_darts)
+	
 	# print "multiprocessing pi approximation= ", pi
-	return Pool_time
+	
+	return multiprocessing_time
 	
 #--------------------------------- IPcluster Method ---------------------------------	
 def sim_for_IP(num_of_throws):
@@ -70,6 +67,7 @@ def sim_for_IP(num_of_throws):
 	from random import uniform
 	from math import sqrt
 	darts_in_circle = 0
+	
 	for i in xrange(num_of_throws):
 		x,y = uniform(0,1),uniform(0,1)
 		if sqrt( (x-.5)**2 + (y-.5)**2 ) < .5 :
@@ -122,22 +120,21 @@ def simple_method(n_of_darts):
 
 
 darts_thrown = []
-simplelist = []
-iplist = []
-poollist = []
-maxtime=10
+simple_list = []
+IP_list = []
+multiprocessing_list = []
+time_limit=10
 
 for dart_throws in [10,100,1000, 10000, 100000,1000000]:
+	
 	darts_thrown.append(dart_throws)
+	simple_data= simple_method(dart_throws); simple_list.append(simple_data)
+	IP_data= IPcluster_method(dart_throws); IP_list.append(IP_data)
+	multi_data= multiprocessing_method(dart_throws); multiprocessing_list.append(multi_data)
 	
-	x = simple_method(dart_throws); simplelist.append(x)
-	
-	y= IPcluster_method(dart_throws); iplist.append(y)
-	
-	z = multiprocessing_method(dart_throws); poollist.append(z)
-	
-	if (x>maxtime) or (y>maxtime) or (z>maxtime):
-		break
+	for data in [simple_data, IP_data, multi_data]:
+		if data > time_limit:
+			break
 		
 print "Finished data collection. Plotting."	
 
@@ -151,9 +148,9 @@ import numpy as np
 from numpy import log
 
 darts = np.array(darts_thrown)
-simple = np.array(simplelist)
-ip = np.array(iplist)
-pool = np.array(poollist)
+simple = np.array(simple_list)
+ip = np.array(IP_list)
+multi = np.array(multiprocessing_list)
 
 
 
@@ -171,7 +168,7 @@ ax.set_xlabel('Number of Darts Thrown')
 
 ax.plot(darts,simple,'m', label='simple method', lw=2, marker='o', ms=5)
 ax.plot(darts,ip,'orange', label ='IPcluster', lw=2, marker='o', ms=5)
-ax.plot(darts,pool,'g', label = 'pool', lw=2,  marker='o', ms=5)
+ax.plot(darts,multi,'g', label = 'multi', lw=2,  marker='o', ms=5)
 
 #ax2 shows dashed lines, xaxis is total darts thrown(logscale), yaxis is rate of throwning: darts/second
 
@@ -183,7 +180,7 @@ ax2.set_ylabel('Rate in Darts/Second: dashed line')
 
 ax2.plot(darts,darts/simple, 'm--')
 ax2.plot(darts,darts/ip,'y--')
-ax2.plot(darts,darts/pool,'g--')
+ax2.plot(darts,darts/multi,'g--')
 
 axline=ax.get_lines()
 
